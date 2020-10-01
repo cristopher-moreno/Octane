c3f0dcb4b00fa1cf98b43e0fa36932bbf3b4fb67
cristopher <cristophermoreno92@gmail.com>
2020-09-30 22:15:13 -0500

    MVP

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form"
+import { Button } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App() {
 
@@ -10,10 +14,24 @@ export default function App() {
   }
 
   return (
+
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="email" placeholder="Email" name="email" ref={register} />
-      <input type="password" placeholder="Password" name="password" ref={register} />
-      <input type="submit" />
+      <div>
+        <input type="number" placeholder="id_trip" name="id_trip" ref={register} required />
+        <input type="date" placeholder="fecha_inicio" name="fecha_inicio" ref={register} required />
+        <input type="date" placeholder="fecha_fin" name="fecha_fin" ref={register} required />
+        <input type="number" placeholder="punto_inicio" name="punto_inicio" ref={register} required />
+        <input type="number" placeholder="punto_fin" name="punto_fin" ref={register} required />
+        <input type="number" step="0.01" placeholder="costo" name="costo" ref={register} required />
+        <input type="number" step="0.001" placeholder="volumen" name="volumen" ref={register} required />
+      </div>
+
+      <Button type="submit" variant="success">OK</Button>{' '}
+
     </form>
+
+
+
+
   )
 }
